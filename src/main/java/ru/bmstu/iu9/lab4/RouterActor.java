f2fe92142168836f910af5afc28b13f096686f6a
AleksandrShteynikov <shtjnk91@gmail.com>
2021-12-02 18:07:35 +0300

    Scripted auto-commit on change (2021-12-02 18:07:35) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab4/RouterActor.java
+++ src/main/java/ru/bmstu/iu9/lab4/RouterActor.java
@@ -3,14 +3,17 @@ package ru.bmstu.iu9.lab4;
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.Props;
+import akka.routing.RoundRobinPool;
 
 public class RouterActor extends AbstractActor {
+    final static int POOL_SIZE = 5;
+
     private ActorRef router;
     private ActorRef storage;
 
     public RouterActor() {
         storage = getContext().actorOf(Props.create(StrorageActor.class));
-        
+        router = getContext().actorOf(new RoundRobinPool(5), );
     }
 
     @Override
