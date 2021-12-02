def85dbdd0f098fe851418e6e1b2766f6287e716
AleksandrShteynikov <shtjnk91@gmail.com>
2021-12-02 16:25:31 +0300

    Scripted auto-commit on change (2021-12-02 16:25:31) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab4/JSTester.java
+++ src/main/java/ru/bmstu/iu9/lab4/JSTester.java
@@ -2,11 +2,13 @@ package ru.bmstu.iu9.lab4;
 
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
 
 public class JSTester {
     static final String AKKA_SYSTEM_NAME = "Akka JS Tester";
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create(AKKA_SYSTEM_NAME);
         final Http http = Http.get(system);
+        final ActorMaterializer 
     }
 }
