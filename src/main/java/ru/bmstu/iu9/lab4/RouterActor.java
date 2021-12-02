14aaa0a8a04967157106641d8dfc07a9d8d9668e
AleksandrShteynikov <shtjnk91@gmail.com>
2021-12-02 18:04:42 +0300

    Scripted auto-commit on change (2021-12-02 18:04:42) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab4/RouterActor.java
+++ src/main/java/ru/bmstu/iu9/lab4/RouterActor.java
@@ -2,13 +2,15 @@ package ru.bmstu.iu9.lab4;
 
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
+import akka.actor.Props;
 
 public class RouterActor extends AbstractActor {
     private ActorRef router;
     private ActorRef storage;
 
     public RouterActor() {
-        storage = getContext().actorOf();
+        storage = getContext().actorOf(Props.create(StrorageActor.class));
+        
     }
 
     @Override
