8ab42987b92debc817af96f5d4c6994fad475213
AleksandrShteynikov <shtjnk91@gmail.com>
2021-12-02 17:21:12 +0300

    Scripted auto-commit on change (2021-12-02 17:21:12) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab4/JSTester.java
+++ src/main/java/ru/bmstu/iu9/lab4/JSTester.java
@@ -12,7 +12,7 @@ import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.Route;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
-import akka.http.javadsl.server.Directives.*;
+import static akka.http.javadsl.server.Directives.*;
 
 import java.io.IOException;
 import java.util.concurrent.CompletionStage;
