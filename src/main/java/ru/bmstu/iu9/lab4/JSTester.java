b9cc55bed1bc2a3e09e5d62d0f61cb041e2d76a0
AleksandrShteynikov <shtjnk91@gmail.com>
2021-12-02 16:51:30 +0300

    Scripted auto-commit on change (2021-12-02 16:51:30) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab4/JSTester.java
+++ src/main/java/ru/bmstu/iu9/lab4/JSTester.java
@@ -1,18 +1,23 @@
 package ru.bmstu.iu9.lab4;
 
 import akka.NotUsed;
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import akka.routing.RoundRobinPool;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
 public class JSTester {
     static final String AKKA_SYSTEM_NAME = "Akka JS Tester";
+    static final String ROUTER_NAME = "router";
+    static final int POOL_SIZE = 5;
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create(AKKA_SYSTEM_NAME);
-        
+        ActorRef router = getContext().actorOf(new RoundRobinPool(POOL_SIZE).props(Props.create(TesterActor.class)), ROUTER_NAME);
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         JSTester instance = new JSTester();
