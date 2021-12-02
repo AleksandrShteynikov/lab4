771614d8e47c1b843ba832a8612ee4e5e94ec6d2
AleksandrShteynikov <shtjnk91@gmail.com>
2021-12-03 00:02:41 +0300

    Scripted auto-commit on change (2021-12-03 00:02:41) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab4/StorageActor.java
+++ src/main/java/ru/bmstu/iu9/lab4/StorageActor.java
@@ -1,11 +1,21 @@
 package ru.bmstu.iu9.lab4;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.List;
 
 public class StorageActor extends AbstractActor {
 
     @Override
     public Receive createReceive() {
-        return null;
+        return ReceiveBuilder.create()
+                .match(TestPackage.Test.class, test -> {
+
+                })
+                .match(String.class, id -> {
+                    getSender().tell(TestPackage, self);
+                }).build();
     }
 }
