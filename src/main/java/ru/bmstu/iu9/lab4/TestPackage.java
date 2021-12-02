0b5b4e83b74e759bb055e125614f9b7d1f10ecfb
AleksandrShteynikov <shtjnk91@gmail.com>
2021-12-02 18:56:23 +0300

    Scripted auto-commit on change (2021-12-02 18:56:23) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab4/TestPackage.java
+++ src/main/java/ru/bmstu/iu9/lab4/TestPackage.java
@@ -1,6 +1,28 @@
 package ru.bmstu.iu9.lab4;
 
+import java.util.List;
+
 public class TestPackage {
+
+    public TestPackage() {
+
+    }
+
+    public String getID() {
+        return null;
+    }
+
+    public String getFuncName() {
+        return null;
+    }
+
+    public String getScript() {
+        return null;
+    }
+
+    public List<Test> getTests() {
+        return null;
+    }
 
     static class Test {
 
