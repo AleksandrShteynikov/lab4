32d531cfe12d1b1f0a0fdd1acc7f3358eefd7e8e
AleksandrShteynikov <shtjnk91@gmail.com>
2021-12-02 16:23:14 +0300

    Scripted auto-commit on change (2021-12-02 16:23:14) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab4/JSTester.java
+++ src/main/java/ru/bmstu/iu9/lab4/JSTester.java
@@ -3,6 +3,7 @@ package ru.bmstu.iu9.lab4;
 import akka.actor.ActorSystem;
 
 public class JSTester {
+    
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("")
     }
