62c923bb7f433be6d76936b79074dda4c57ee098
AleksandrShteynikov <shtjnk91@gmail.com>
2021-12-02 16:48:16 +0300

    Scripted auto-commit on change (2021-12-02 16:48:16) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab4/JSTester.java
+++ src/main/java/ru/bmstu/iu9/lab4/JSTester.java
@@ -1,10 +1,12 @@
 package ru.bmstu.iu9.lab4;
 
+import akka.NotUsed;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 public class JSTester {
     static final String AKKA_SYSTEM_NAME = "Akka JS Tester";
@@ -13,6 +15,7 @@ public class JSTester {
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         JSTester instance = new JSTester();
-        final Flow<HttpRequest, HttpResponse>
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = instance.createRoute(system).flow(system, materializer);
+        
     }
 }
