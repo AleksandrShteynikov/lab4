b2caecafc52d78059619362d7094146ddebaa335
AleksandrShteynikov <shtjnk91@gmail.com>
2021-12-03 00:11:38 +0300

    Scripted auto-commit on change (2021-12-03 00:11:38) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab4/SingleTest.java
+++ src/main/java/ru/bmstu/iu9/lab4/SingleTest.java
@@ -2,8 +2,31 @@ package ru.bmstu.iu9.lab4;
 
 public class SingleTest {
 
+    private final String id;
+    private final String script;
+    private final String funcName;
+    private final TestPackage.Test test;
 
     public SingleTest(String id, String script, String funcName, TestPackage.Test test) {
+        this.id = id;
+        this.script = script;
+        this. funcName = funcName;
+        this.test = test;
+    }
 
+    public String getFuncName() {
+        return funcName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public TestPackage.Test getTest() {
+        return test;
+    }
+
+    public String getScript() {
+        return script;
     }
 }
