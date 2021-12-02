f22a2b561fe88c2f9ab3c073728689a8ab82df88
AleksandrShteynikov <shtjnk91@gmail.com>
2021-12-02 18:14:21 +0300

    Scripted auto-commit on change (2021-12-02 18:14:21) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab4/RouterActor.java
+++ src/main/java/ru/bmstu/iu9/lab4/RouterActor.java
@@ -22,6 +22,6 @@ public class RouterActor extends AbstractActor {
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match()
-                .match().build();
+                .match(String.class ).build();
     }
 }
