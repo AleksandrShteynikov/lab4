c3ec3829a3008dce8da9896434566f108b527198
AleksandrShteynikov <shtjnk91@gmail.com>
2021-12-02 17:15:30 +0300

    Scripted auto-commit on change (2021-12-02 17:15:30) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab4/JSTester.java
+++ src/main/java/ru/bmstu/iu9/lab4/JSTester.java
@@ -16,6 +16,8 @@ import akka.stream.javadsl.Flow;
 import java.io.IOException;
 import java.util.concurrent.CompletionStage;
 
+import static akka.http.javadsl.server.Directives.route;
+
 public class JSTester {
     static final String AKKA_SYSTEM_NAME = "Akka JS Tester";
     static final String ROUTER_NAME = "router";
@@ -35,6 +37,6 @@ public class JSTester {
         binding.thenCompose(ServerBinding::unbind).thenAccept(unbound->system.terminate());
     }
     private Route createRoute(ActorSystem system) {
-        
+        return route();
     }
 }
