3c7d71cead9a3f646f6d139b618640483636c2a3
AleksandrShteynikov <shtjnk91@gmail.com>
2021-12-03 11:18:04 +0300

    Scripted auto-commit on change (2021-12-03 11:18:04) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab4/ReturnResult.java
+++ src/main/java/ru/bmstu/iu9/lab4/ReturnResult.java
@@ -1,2 +1,17 @@
-package ru.bmstu.iu9.lab4;public class ReturnResult {
+package ru.bmstu.iu9.lab4;
+
+import java.util.List;
+
+public class ReturnResult {
+    private final String id;
+    private final List<Test> tests;
+
+    public ReturnResult(String id, List<Test> tests) {
+        this.tests = tests;
+        this.id = id;
+    }
+
+    static class Test {
+
+    }
 }
